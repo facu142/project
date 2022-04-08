5c6504f1e9a74fe199ebfd9360145a3c9bdbb251
Facundo Montenegro <facumonte29@gmail.com>
2022-04-07 23:39:32 -0300

    agrega seeders proyectos

--- seeders/20220408021347-create-projects.js
+++ seeders/20220408021347-create-projects.js
@@ -1,11 +1,23 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Projects', [{
-      name: 'Build an app',
-      userId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    return queryInterface.bulkInsert('Projects', [
+      {
+        name: 'Project 1',
+        description: 'Project 1 description',
+        status: true,
+        projectManagerId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Project 2',
+        description: 'Project 2 description',
+        status: true,
+        projectManagerId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Projects', null, {});
